f9051e46c3f2af3e8ee6359efa6e07b3f052090b
RianVitor26 <rianvitorlhe@gmail.com>
2022-10-23 03:39:12 -0300

    fix: fix bug on toastify lib

--- portifolio/src/components/Toastify/index.tsx
+++ portifolio/src/components/Toastify/index.tsx
@@ -1,20 +1,19 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Container } from './styles'
+import { Container } from "./styles";
 
-export const Toastify = () =>  {
-    const notify = () =>
-      toast("What's Up ! Welcome to my portifolio 🚀", {
-        position: "top-left",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
-  
+export const Toastify = () => {
+  const notify = () =>
+    toast("What's Up ! Welcome to my portifolio 🚀", {
+      position: "top-left",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
 
   return (
     <Container>
@@ -22,4 +21,4 @@ export const Toastify = () =>  {
       <ToastContainer />
     </Container>
   );
-}
+};
